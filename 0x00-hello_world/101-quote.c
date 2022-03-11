bdbb1a0d3a66e617d768dbd722a776529f89f0d9
jobootwori <jobootwori@gmail.com>
2022-03-11 09:55:19 +0300

    Prints a message to the user while returning 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, " - Dora Korpar");
+	fprintf(stderr, " - Dora Korpar,");
 	fprintf(stderr, " 2015-10-19\n");
-	return(1);
+	return (1);
 }
