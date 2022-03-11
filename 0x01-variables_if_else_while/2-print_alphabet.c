6462c5057edc83f75e3346628c9192624b5dd454
jobootwori <jobootwori@gmail.com>
2022-03-11 21:59:12 +0300

    Prints alphabets i lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,12 @@
 int main(void)
 {
 	int x;
+
 	for (x = 'A'; x <= 'Z'; x++)
 	{
-		int lower_x = tolower(x); 
+		int lower_x = tolower(x);
 		putchar(lower_x);
 	}
+	putchar('\n');
 	return (0);
 }
