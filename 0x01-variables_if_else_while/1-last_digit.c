a642a39c49fc7ab84988213ce2edffacfd1a797d
jobootwori <jobootwori@gmail.com>
2022-03-11 21:11:02 +0300

    Shows whether the last no of an int meets some conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	printf("Last digit of %d is %d ", n, n % 10);
-	if (n %10 > 5)
+	if (n % 10 > 5)
 	{
 		printf("and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("and is zero\n");
+		printf("and is 0\n");
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
