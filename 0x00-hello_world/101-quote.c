a5640fab24a7d8a84d9d626e59fba205c3260571
jobootwori <jobootwori@gmail.com>
2022-03-11 09:38:21 +0300

    Prints a message to the user while returning 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase 'Programming...
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	fprinf(stderr, "and that piece of art is useful\", - Dora Korpar, 2015-10-19\n");
+	return(1)
+}
