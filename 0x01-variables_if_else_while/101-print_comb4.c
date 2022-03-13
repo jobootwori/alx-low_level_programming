935c45ccdbe7074aaf7753cca1ffdc9cd2ce8dde
jobootwori <jobootwori@gmail.com>
2022-03-13 18:16:00 +0300

    It prints a combination of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int s, b;
+
+	for (s = 48; s <= 57; s++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (t = 48; t <=57; t++) 
+			{
+				if (t < b > s)
+				{
+					putchar(t);
+					putchar(s);
+					putchar(b)
+
+						if (t == 55 && b == 57 && s == 56)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');				
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
