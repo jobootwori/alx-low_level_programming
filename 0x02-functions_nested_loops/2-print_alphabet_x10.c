618551eeb82ff55ea10e4bc47d2ea303fbbd7ffa
jobootwori <jobootwori@gmail.com>
2022-03-15 19:35:41 +0300

    Prints alphabets from a - z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters a -z
+  */
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+		int a = 97;
+		while (a <123
+		{
+			int a = 97;
+			while (a < 123)
+			{
+
+
+
+			_putchar(a);
+			a++;
+			if (a == 123)
+
+
+			}
+		}	
+	}
+}
