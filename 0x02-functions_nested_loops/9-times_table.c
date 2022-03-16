878732fd2c30e4806438549c632e601a135d5616
jobootwori <jobootwori@gmail.com>
2022-03-16 11:45:34 +0300

    a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * times_table - Prints 9 times timetable
+ *
+ */
+void times_table(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if n == 57
+			break;
+		_putchar(n);
+		_putchar(' ');
+	}
+}
