8784f39a194485073da78098a72cafa3e1a809a1
jobootwori <jobootwori@gmail.com>
2022-03-18 20:04:00 +0300

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,14 @@ void print_most_numbers(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		if (c == 50 || c == 52)
+		if (c != 50 && c != 52)
 		{
-			break;
+			_putchar(c);
 		}
 		else
 		{
-			continue;
+			break;
 		}
-		_putchar(c);
 	}
 	_putchar('\n');
 }
