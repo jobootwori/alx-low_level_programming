bb72c2921424f34e3c5b0f471d0e7b47dd1e20fe
jobootwori <jobootwori@gmail.com>
2022-03-21 22:51:02 +0300

    a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Swaps the values of two integers
+ *
+ * @s: Input Integer
+ */
+int _strlen(char *s)
+{
+	char *str[], i;
+
+	str[] = s;
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(i);
+	}
+}
