a531648ac2ade1933b85cc3b7cc400b11e2e5604
jobootwori <jobootwori@gmail.com>
2022-03-23 19:46:15 +0300

    a function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses a string
+ *
+ *@a: Input
+ *@n: INput
+ *Return: Returns value
+ */
+void reverse_array(int *a, int n);
+{
+	int k;
+
+	for (k = (n - 1); k => 0; k++)
+	{
+		return a[k];
+	}
+	_putchar('\n');
+}
