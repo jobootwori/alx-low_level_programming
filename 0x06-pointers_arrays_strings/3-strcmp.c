801af7acb21fbd9af0d84e6fc9b0ab0b8f447076
jobootwori <jobootwori@gmail.com>
2022-03-24 19:31:44 +0300

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int k;
 
-	for (k = 0; s1[k] == s2[k] && s1[k] == '\0'; k++)
+	for (k = 0; (s1[k] == s2[k]) && (s1[k] == '\0'); k++)
 
 		if(s1[k] < s2[k])
 		{
