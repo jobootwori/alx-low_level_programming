4e1c857ca9b0dc942568018a7095a4c2845060d1
jobootwori <jobootwori@gmail.com>
2022-03-26 12:43:30 +0300

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates two strings
+ * _strncat - concatenates two strings
  *
  *@dest: Input
  *@src: INput
  *@n: input
+ * Return: Returna a value
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -13,10 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (k = 0; (*(dest + k)) != '\0'; k++)
 	
-	for (i = 0; (*src + i)) != '\0'; i++, k++)
+	for (i = 0; (*(src + i)) != '\0'; i++, k++)
 	{
 		while (i < n)
+		{
 			*(dest + k) = *(src + i);
+		}
 	}
 	return(dest);
 	_putchar('\n');
