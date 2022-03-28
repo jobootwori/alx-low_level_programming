a1e3eb437bc2d2e6bf1553f46e22d3474ea0478f
jobootwori <jobootwori@gmail.com>
2022-03-28 20:38:55 +0300

    a function that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,14 +11,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	/* Typecast src and dest addresses to (char *) */
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
+	unsigned int i;
 
-	/* Copy contents of src[] to dest[] */
-	for (int i = 0; i < n; i++)
-	{
-		cdest[i] = csrc[i];
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
