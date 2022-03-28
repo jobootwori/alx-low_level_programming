84563f6e9295eddd7932b4fd7cbed787f6a82e7c
jobootwori <jobootwori@gmail.com>
2022-03-28 18:23:43 +0300

    a function that prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 /**
  *  print_chessboard-  a function that prints the chessboard
  *
- * 
+ *@a: array 
  *
  * Return: return 0
  *
@@ -13,9 +13,9 @@ void print_chessboard(char (*a)[8])
 
 	int i, j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
