dc1cb3791c8f77e8804e2a0e1c4153d5cf3cc41f
jobootwori <jobootwori@gmail.com>
2022-03-30 10:54:43 +0300

    a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: Input
+ * @src: INput
+ * @n: Input
+ * Return: returns dest
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
+		_putchar(s[i]);
+	i++;
+	_puts_recursion(s[i]);
+}
