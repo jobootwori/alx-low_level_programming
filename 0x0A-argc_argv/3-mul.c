7b571b2f93216d286688819515df0be2b426f0f7
jobootwori <jobootwori@gmail.com>
2022-04-01 21:02:43 +0300

    a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of argumnets
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc > 1 && argc < 4)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
