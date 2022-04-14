e2cf40ddce3c611e11d649026c2177c80e2538a5
jobootwori <jobootwori@gmail.com>
2022-04-14 22:06:09 +0300

    operations header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,23 @@
-#ifndef _variadic_functions_h
-#define _variadic_functions_h
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 
-/*
+/**
  * Function prototypes
  */
+
+typedef struct check
+{
+	char *chk;
+	void (*f)(va_list list);
+} check_t;
 
 #include <stdarg.h>
 #include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
