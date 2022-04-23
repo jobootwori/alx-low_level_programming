8f172afa9f731593cfe1f30d2c8f68483dd1aee5
jobootwori <jobootwori@gmail.com>
2022-04-23 15:40:28 +0300

    Made sum unsigned int

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
-	va_list list;
-
-	va_start(list, n);
+	unsigned int i; 
+	sum = 0;
 	
-	if (n == '0')
+	va_list list;
+	
+	if (n == 0)
 	{
 		return (0);
 	}
 
+	va_start(list, n);
+	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
