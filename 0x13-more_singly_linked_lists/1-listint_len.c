604c09bca1b4a68af72b006ca64b07251af0505e
jobootwori <jobootwori@gmail.com>
2022-04-25 21:07:20 +0300

    A function that returns no of items

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+**
+ * print_listint - prints all the elements of a list
+ * @h: the head of the list to print from
+ * Return: the number of nodes
+ */
+ size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+	
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
