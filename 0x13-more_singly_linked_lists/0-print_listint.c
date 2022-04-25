702b5e3c7eae0313e3b0537f32ece3213ad04674
jobootwori <jobootwori@gmail.com>
2022-04-25 20:49:37 +0300

    A function that prints all items in struct returning no of items

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
-**
+/**
  * print_listint - prints all the elements of a list
  * @h: the head of the list to print from
  * Return: the number of nodes
  */
- size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	int counter = 0;
+	size_t counter = 0;
 
 	while (h != NULL)
 	{
-		printf(" %d ", h->n):
+		printf(" %d ", h->n);
 		h = h->next;
 		counter++;
 	}
