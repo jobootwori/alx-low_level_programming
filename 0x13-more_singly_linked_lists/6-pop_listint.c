2e08b0a090f402e5c33fc2c4aaf9966468d592cd
jobootwori <jobootwori@gmail.com>
2022-04-26 21:20:35 +0300

    a function that deletes the head node of a listint_t linked list, and returns the head node’s data (n).

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - a function that deletes the head node of a listint_t 
+ * linked list, and returns the head node’s d
+ *
+ * @head: the head node of the list
+ *
+ * Return: the heads' node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int result = (*head)->n;
+
+	if (*head == NULL)
+		return (0);
+	
+	printf("%d\n",(*head)->n);
+	(*head) = (*head)->next;
+	
+	free(temp);
+
+	return (result);
+}
