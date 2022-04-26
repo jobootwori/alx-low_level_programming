fcc08a0c992c1ff1dfd9b69152948773958c942e
jobootwori <jobootwori@gmail.com>
2022-04-26 15:20:24 +0300

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,15 +2,15 @@
 
 /**
  * free_listint - adds a new node to end of the listint_t list
- * 
+ *
  * @head: the head node of the list
- * 
+ *
  */
 
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	
+
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
