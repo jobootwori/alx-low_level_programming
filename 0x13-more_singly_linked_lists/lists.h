665954910351bad18fbde9da4a6c0a56e518c226
jobootwori <jobootwori@gmail.com>
2022-04-26 11:36:37 +0300

    Removed whitespaces

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,8 +12,8 @@
 
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -22,8 +22,8 @@ typedef struct listint_s
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
