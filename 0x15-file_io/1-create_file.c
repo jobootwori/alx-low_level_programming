84c71f9f808c43646c15b47655e1998955f20371
jobootwori <jobootwori@gmail.com>
2022-05-03 21:43:51 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  *
  * create_file - creates a file and fills it with text
- * 
+ *
  * @filename: name of the file to create
  * @text_content: text to write in the file
  *
@@ -18,14 +18,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		while (text_content[s])
 			s++;
 
 		t = write(fd, text_content, s);
-		if(t != s)
+		if (t != s)
 			return (-1);
 	}
 
