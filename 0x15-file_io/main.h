4e4b9e8788ef35f8f89bddd4f471844d2362f291
jobootwori <jobootwori@gmail.com>
2022-05-03 17:44:09 +0300

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
